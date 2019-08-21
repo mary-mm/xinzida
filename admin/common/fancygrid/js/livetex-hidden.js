36bf5db6da2e1c16323a164145fa1493be4df54f
chenguoying <724981586@qq.com>
2019-08-21 11:19:28 +0800

    8.21

---
+++ admin/common/fancygrid/js/livetex-hidden.js
@@ -0,0 +1,23 @@
+/*
+window.ltLang = 'en';
+
+window.liveTex = true,
+window.liveTexID = 140876,
+window.liveTex_object = true;
+
+var LiveTex = {
+  onLiveTexReady: function(){
+    LiveTex.hideLabel();
+  }
+};
+
+(function() {
+var t = document['createElement']('script');
+t.type ='text/javascript';
+t.async = true;
+t.src = '//cs15.livetex.ru/js/client.js';
+var c = document['getElementsByTagName']('script')[0];
+if ( c ) c['parentNode']['insertBefore'](t, c);
+else document['documentElement']['firstChild']['appendChild'](t);
+})();
+*/
